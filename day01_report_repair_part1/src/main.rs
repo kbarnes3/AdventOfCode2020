1657553c3c605094e67010389c4f2f9fde2755e7
Kevin Barnes <kbarnes3@gmail.com>
2020-12-01 17:22:56 -0800

    Solving part 1

--- day01_report_repair_part1/src/main.rs
+++ day01_report_repair_part1/src/main.rs
@@ -1,5 +1,25 @@
-use day01_report_repair_common::SAMPLE_DATA;
+#[allow(unused_imports)]
+use day01_report_repair_common::{TARGET_NUMBER, SAMPLE_DATA, REAL_DATA};
 
 fn main() {
-    println!("Hello, world! {}", SAMPLE_DATA);
+    match do_work(&REAL_DATA) {
+        Some(answer) => println!("{}", answer),
+        None => println!("Sad"),
+    }
+}
+
+fn do_work(data: &[u32]) -> Option<u32> {
+    for (i, outer_element) in data.iter().enumerate() {
+        for (j, inner_element) in data.iter().enumerate() {
+            if i != j {
+                let sum = outer_element + inner_element;
+                if sum == TARGET_NUMBER {
+                    let product = outer_element * inner_element;
+                    return Some(product);
+                }
+            }
+        }
+    }
+
+    None
 }
