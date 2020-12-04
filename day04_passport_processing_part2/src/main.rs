d666b226311ec7513c73bfb8c6b43ca0aa3a2e88
Kevin Barnes <kbarnes3@gmail.com>
2020-12-04 15:56:32 -0800

    Sarting Day 04 Part 2

---
+++ day04_passport_processing_part2/src/main.rs
@@ -0,0 +1,64 @@
+#[allow(unused_imports)]
+use day04_passport_processing_common::{SAMPLE_DATA, REAL_DATA};
+
+fn main() {
+    let result = do_work(&REAL_DATA);
+    println!("{}", result);
+}
+
+fn do_work(data: &[&str]) -> usize {
+    let mut valid_count: usize = 0;
+    let mut start_line: usize = 0;
+    let mut end_line: usize;
+
+    for (i, line) in data.iter().enumerate() {
+        if line.trim() == "" {
+            end_line = i;
+            if process_passport(&data[start_line..end_line+1]) {
+                valid_count += 1;
+            }
+            start_line = end_line + 1;
+        }
+    }
+
+    if start_line < data.len() {
+        end_line = data.len() - 1;
+        if process_passport(&data[start_line..end_line+1]) {
+            valid_count += 1;
+        }
+    }
+    
+    valid_count
+}
+
+fn process_passport(data: &[&str]) -> bool {
+    let mut byr = false;
+    let mut iyr = false;
+    let mut eyr = false;
+    let mut hgt = false;
+    let mut hcl = false;
+    let mut ecl = false;
+    let mut pid = false;
+
+    for line in data.iter() {
+        for field in line.trim().split_whitespace() {
+            if field.starts_with("byr") {
+                byr = true;
+            } else if field.starts_with("iyr") {
+                iyr = true;
+            } else if field.starts_with("eyr") {
+                eyr = true;
+            } else if field.starts_with("hgt") {
+                hgt = true;
+            } else if field.starts_with("hcl") {
+                hcl = true;
+            } else if field.starts_with("ecl") {
+                ecl = true;
+            } else if field.starts_with("pid") {
+                pid = true;
+            }
+        }
+    }
+
+    byr && iyr && eyr && hgt && hcl && ecl && pid
+}
