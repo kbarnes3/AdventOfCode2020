1c5bb084e99dbedaf554d74cc6c5b6ea60e132dd
Kevin Barnes <kbarnes3@gmail.com>
2020-12-03 17:20:26 -0800

    Sample data for Day 03 Part 1 passes

--- day03_toboggan_trajectory_common/src/lib.rs
+++ day03_toboggan_trajectory_common/src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[derive(Copy, Clone)]
+pub enum Space {
+    Open,
+    Tree
 }
+
+use Space::{Open, Tree};
+
+// Convert with:
+// '<,'>s/^/    [ /
+// '<,'>s/$/],
+// '<,'>s/\./Open, /g
+// '<,'>s/#/Tree, /g
+
+pub const SAMPLE_DATA: [[Space; 11]; 11] = [
+    [ Open, Open, Tree, Tree, Open, Open, Open, Open, Open, Open, Open, ],
+    [ Tree, Open, Open, Open, Tree, Open, Open, Open, Tree, Open, Open, ],
+    [ Open, Tree, Open, Open, Open, Open, Tree, Open, Open, Tree, Open, ],
+    [ Open, Open, Tree, Open, Tree, Open, Open, Open, Tree, Open, Tree, ],
+    [ Open, Tree, Open, Open, Open, Tree, Tree, Open, Open, Tree, Open, ],
+    [ Open, Open, Tree, Open, Tree, Tree, Open, Open, Open, Open, Open, ],
+    [ Open, Tree, Open, Tree, Open, Tree, Open, Open, Open, Open, Tree, ],
+    [ Open, Tree, Open, Open, Open, Open, Open, Open, Open, Open, Tree, ],
+    [ Tree, Open, Tree, Tree, Open, Open, Open, Tree, Open, Open, Open, ],
+    [ Tree, Open, Open, Open, Tree, Tree, Open, Open, Open, Open, Tree, ],
+    [ Open, Tree, Open, Open, Tree, Open, Open, Open, Tree, Open, Tree, ],
+];
--- day03_toboggan_trajectory_part1/src/main.rs
+++ day03_toboggan_trajectory_part1/src/main.rs
@@ -1,3 +1,36 @@
+#[allow(unused_imports)]
+use day03_toboggan_trajectory_common::{Space, SAMPLE_DATA};
+
 fn main() {
-    println!("Hello, world!");
+    let result = do_work(&SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work<Grid: AsRef<[Row]>, Row: AsRef<[Space]>>(data: Grid) -> u32
+{
+    let mut tree_count = 0;
+    const X_INCREMENT: usize = 3;
+    const Y_INCREMENT: usize = 1;
+    let mut x: usize = X_INCREMENT;
+    let mut y: usize = Y_INCREMENT;
+
+    let max_x = data.as_ref()[0].as_ref().len();
+    let max_y = data.as_ref().len();
+
+    while y < max_y {
+        while x >= max_x {
+            x -= max_x;
+        }
+
+        let space = data.as_ref()[y].as_ref()[x];
+        if let Space::Tree = space {
+            tree_count += 1;
+        }
+
+        x += X_INCREMENT;
+        y += Y_INCREMENT;
+
+    }
+
+    tree_count
 }
