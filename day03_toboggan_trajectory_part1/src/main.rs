fa38bc382e903a39b7a777ff874d20632df1ad9d
Kevin Barnes <kbarnes3@gmail.com>
2020-12-03 17:30:44 -0800

    Solving Day 03 Part 1

--- day03_toboggan_trajectory_part1/src/main.rs
+++ day03_toboggan_trajectory_part1/src/main.rs
@@ -1,8 +1,8 @@
 #[allow(unused_imports)]
-use day03_toboggan_trajectory_common::{Space, SAMPLE_DATA};
+use day03_toboggan_trajectory_common::{Space, SAMPLE_DATA, REAL_DATA};
 
 fn main() {
-    let result = do_work(&SAMPLE_DATA);
+    let result = do_work(&REAL_DATA);
     println!("{}", result);
 }
 
