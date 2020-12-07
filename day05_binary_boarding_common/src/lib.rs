096ada6819cc2e972c01ccecaac4cf5cc090d4f6
Kevin Barnes <kbarnes3@gmail.com>
2020-12-07 13:26:36 -0800

    Day 05 Part 1 sample passes

--- day05_binary_boarding_common/src/lib.rs
+++ day05_binary_boarding_common/src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Substitute with:
+// '<,'>s/F/0/ge | '<,'>s/B/1/ge | '<,'>s/L/0/ge | '<,'>s/R/1/ge
+
+pub const SAMPLE_DATA: [u16; 1] = [
+    0b0101100101,
+];
+
--- day05_binary_boarding_part1/src/main.rs
+++ day05_binary_boarding_part1/src/main.rs
@@ -1,3 +1,12 @@
+#[allow(unused_imports)]
+use day05_binary_boarding_common::{SAMPLE_DATA};
+
 fn main() {
-    println!("Hello, world!");
+    let result = do_work(&SAMPLE_DATA);
+
+    println!("{}", result);
+}
+
+fn do_work(data: &[u16]) -> u16 {
+    *data.iter().max().unwrap()
 }
