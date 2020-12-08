bd11b2c8883b97bae4dbda4910bcb99fc9a13aa2
Kevin Barnes <kbarnes3@gmail.com>
2020-12-08 13:27:31 -0800

    Day 06 Part 2 works with sample data

--- day06_custom_customs_part2/src/main.rs
+++ day06_custom_customs_part2/src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use day06_custom_customs_common::{SAMPLE_DATA, REAL_DATA};
 
 fn main() {
-    let result = do_work(&REAL_DATA);
+    let result = do_work(&SAMPLE_DATA);
     println!("{}", result);
 }
 
@@ -28,13 +28,22 @@ fn do_work(data: &[&str]) -> usize {
 }
 
 fn process_group(group: &[&str]) -> usize {
-    let mut yes_answers = HashSet::new();
+    let mut yes_answers = HashMap::<char, usize>::new();
 
     for person in group.iter() {
         for yes_answer in person.trim().chars() {
-            yes_answers.insert(yes_answer);
+            let counter = yes_answers.entry(yes_answer).or_insert(0);
+            *counter += 1;
         }
     }
 
-    yes_answers.len()
+    let group_size = group.iter().len();
+    let mut all_match = 0;
+    for (_answer, count) in yes_answers.iter() {
+        if *count == group_size {
+            all_match += 1;
+        }
+    }
+
+    all_match
 }
