14d584ef6d7ddf6028498db5a4ceec43920bdb6b
Kevin Barnes <kbarnes3@gmail.com>
2020-12-08 11:56:25 -0800

    Solving Day 06 Part 1 sample data

--- day06_custom_customs_common/src/lib.rs
+++ day06_custom_customs_common/src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Transform with
+// '<,'>s/^/    "/ | '<,'>s/$/",/
+
+pub const SAMPLE_DATA: [&str; 15] = [
+    "abc",
+    "",
+    "a",
+    "b",
+    "c",
+    "",
+    "ab",
+    "ac",
+    "",
+    "a",
+    "a",
+    "a",
+    "a",
+    "",
+    "b",
+];
--- day06_custom_customs_part1/src/main.rs
+++ day06_custom_customs_part1/src/main.rs
@@ -1,3 +1,40 @@
+use std::collections::HashSet;
+#[allow(unused_imports)]
+use day06_custom_customs_common::{SAMPLE_DATA};
+
 fn main() {
-    println!("Hello, world!");
+    let result = do_work(&SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work(data: &[&str]) -> usize {
+    let mut total: usize = 0;
+    let mut start_line: usize = 0;
+    let mut end_line: usize;
+
+    for (i, line) in data.iter().enumerate() {
+        if line.trim() == "" {
+            end_line = i;
+            total += process_group(&data[start_line..end_line]);
+            start_line = end_line + 1;
+        }
+    }
+
+    if start_line < data.len() {
+        total += process_group(&data[start_line..data.len()]);
+    }
+    
+    total
+}
+
+fn process_group(group: &[&str]) -> usize {
+    let mut yes_answers = HashSet::new();
+
+    for person in group.iter() {
+        for yes_answer in person.trim().chars() {
+            yes_answers.insert(yes_answer);
+        }
+    }
+
+    yes_answers.len()
 }
