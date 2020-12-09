832983693bab1e5298610e2c71478c691a730135
Kevin Barnes <kbarnes3@gmail.com>
2020-12-08 18:20:20 -0800

    Solving Day 07 Part 2

--- day07_handy_haversacks_part2/src/main.rs
+++ day07_handy_haversacks_part2/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use day07_handy_haversacks_common::{BagInfo, TARGET_BAG, SAMPLE_DATA, REAL_DATA};
 
@@ -8,24 +8,36 @@ fn main() {
 }
 
 fn do_work(data: &[BagInfo]) -> usize {
-    let mut made_progress = true;
-    let mut valid_bags = HashSet::new();
+    let mut bag_data = HashMap::new();
 
-    while made_progress {
-        made_progress = false;
-
-        for bag in data.iter() {
-            if !valid_bags.contains(bag.color) {
-                for contained_bag in bag.contains.iter() {
-                    if (contained_bag.color == TARGET_BAG) ||
-                        (valid_bags.contains(contained_bag.color)) {
-                        valid_bags.insert(bag.color);
-                        made_progress = true;
-                    }
-                }
-            }
-        }
+    for bag in data.iter() {
+        bag_data.insert(bag.color, bag);
     }
 
-    valid_bags.len()
+    let mut known_contained_bags = HashMap::new();
+
+    get_contained_bags(TARGET_BAG, &bag_data, &mut known_contained_bags)
 }
+
+fn get_contained_bags(
+    target_bag: &'static str,
+    all_bag_data: &HashMap::<&str, &BagInfo>,
+    known_counts: &mut HashMap::<&str, usize>) -> usize {
+
+    if let Some(count) = known_counts.get(target_bag) {
+        return *count;
+    }
+
+    let bag_data = all_bag_data.get(target_bag).expect(&format!("Missing data for {}", target_bag));
+    let mut contained_count = 0;
+
+    for contained_bag in bag_data.contains.iter() {
+        let mut count = get_contained_bags(contained_bag.color, all_bag_data, known_counts);
+        count += 1; // Include the bag itself
+        contained_count += count * contained_bag.count;
+    }
+
+    known_counts.insert(target_bag, contained_count);
+    contained_count
+}
+
