72d15b0d818b52017711cdb9ef143068841a97ae
Kevin Barnes <kbarnes3@gmail.com>
2020-12-09 14:02:13 -0800

    Solving Day 08 Part 1 sample data

--- day08_handheld_halting_common/src/lib.rs
+++ day08_handheld_halting_common/src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub enum Instruction {
+    Noop,
+    Accumulate(isize),
+    Jump(isize)
 }
+
+use Instruction::*;
+
+// Substitute with
+// '<,'>s/+//e | '<,'>s/^nop .*$/    Noop,/e | '<,'>s/^acc \(.*\)$/    Accumulate(\1),/e | '<,'>s/^jmp \(.*\)/    Jump(\1),/e
+pub const SAMPLE_DATA: [Instruction; 9] = [
+    Noop,
+    Accumulate(1),
+    Jump(4),
+    Accumulate(3),
+    Jump(-3),
+    Accumulate(-99),
+    Accumulate(1),
+    Jump(-4),
+    Accumulate(6),
+];
--- day08_handheld_halting_part1/src/main.rs
+++ day08_handheld_halting_part1/src/main.rs
@@ -1,3 +1,51 @@
+use std::collections::HashSet;
+#[allow(unused_imports)]
+use day08_handheld_halting_common::{Instruction, SAMPLE_DATA};
+
 fn main() {
-    println!("Hello, world!");
+    let result = do_work(&SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work(data: &'static [Instruction]) -> isize {
+    let mut computer = Computer::new(data);
+    computer.run()
+}
+
+struct Computer {
+    instructions: &'static [Instruction],
+    instruction_pointer: usize,
+    accumulator: isize,
+    seen_instructions: HashSet::<usize>,
+}
+
+impl Computer {
+    pub fn new(instructions: &'static [Instruction]) -> Computer {
+        Computer {
+            instructions,
+            instruction_pointer: 0,
+            accumulator: 0,
+            seen_instructions: HashSet::new()
+        }
+    }
+
+    pub fn run(&mut self) -> isize {
+        loop {
+            if self.seen_instructions.contains(&self.instruction_pointer) {
+                return self.accumulator;
+            }
+            self.seen_instructions.insert(self.instruction_pointer);
+
+            match self.instructions[self.instruction_pointer] {
+                Instruction::Noop => self.instruction_pointer += 1,
+                Instruction::Accumulate(value) => {
+                    self.accumulator += value;
+                    self.instruction_pointer += 1
+                },
+                Instruction::Jump(value) => {
+                    self.instruction_pointer = (self.instruction_pointer as isize + value) as usize
+                }
+            }
+        }
+    }
 }
