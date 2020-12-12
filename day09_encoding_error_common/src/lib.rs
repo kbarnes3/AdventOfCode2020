f530981ace2718f7366e7aaefcc5b3c8bfef04ae
Kevin Barnes <kbarnes3@gmail.com>
2020-12-11 16:02:39 -0800

    Day 09 Part 1 sample works

--- day09_encoding_error_common/src/lib.rs
+++ day09_encoding_error_common/src/lib.rs
@@ -1,1 +1,24 @@
-pub const HELLO: bool = true;
+pub const SAMPLE_DATA_PREAMBLE: usize = 5;
+
+pub const SAMPLE_DATA: [u32; 20] = [
+    35,
+    20,
+    15,
+    25,
+    47,
+    40,
+    62,
+    55,
+    65,
+    95,
+    102,
+    117,
+    150,
+    182,
+    127,
+    219,
+    299,
+    277,
+    309,
+    576,
+];
--- day09_encoding_error_part1/src/main.rs
+++ day09_encoding_error_part1/src/main.rs
@@ -1,6 +1,34 @@
 #[allow(unused_imports)]
-use day09_encoding_error_common::{HELLO};
+use day09_encoding_error_common::{SAMPLE_DATA_PREAMBLE, SAMPLE_DATA};
 
 fn main() {
-    println!("Hello, world!");
+    let result = do_work(&SAMPLE_DATA, SAMPLE_DATA_PREAMBLE);
+    match result {
+        Some(value) => println!("{}", value),
+        None => println!("Error")
+    }
+}
+
+fn do_work(data: &[u32], preamble_len: usize) -> Option<u32> {
+    for i in preamble_len..data.len() {
+        let target = data[i];
+        let candidates = &data[i - preamble_len..i];
+        if !is_sum(target, candidates) {
+            return Some(target);
+        }
+    }
+    None
+}
+
+fn is_sum(target: u32, candidates: &[u32]) -> bool {
+    for (i, first) in candidates.iter().enumerate() {
+        let second_candidates = &candidates[i + 1..];
+        for (_, second) in second_candidates.iter().enumerate() {
+            if first + second == target {
+                return true;
+            }
+        }
+    }
+
+    false
 }
