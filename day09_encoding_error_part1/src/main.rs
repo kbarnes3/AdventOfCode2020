e36980e2e7afbbf2d4682edbf924d8dcc2a46947
Kevin Barnes <kbarnes3@gmail.com>
2020-12-11 16:05:34 -0800

    Solving Day 09 Part 1

--- day09_encoding_error_part1/src/main.rs
+++ day09_encoding_error_part1/src/main.rs
@@ -1,15 +1,15 @@
 #[allow(unused_imports)]
-use day09_encoding_error_common::{SAMPLE_DATA_PREAMBLE, SAMPLE_DATA};
+use day09_encoding_error_common::{SAMPLE_DATA_PREAMBLE, SAMPLE_DATA, REAL_DATA_PREAMBLE, REAL_DATA};
 
 fn main() {
-    let result = do_work(&SAMPLE_DATA, SAMPLE_DATA_PREAMBLE);
+    let result = do_work(&REAL_DATA, REAL_DATA_PREAMBLE);
     match result {
         Some(value) => println!("{}", value),
         None => println!("Error")
     }
 }
 
-fn do_work(data: &[u32], preamble_len: usize) -> Option<u32> {
+fn do_work(data: &[u64], preamble_len: usize) -> Option<u64> {
     for i in preamble_len..data.len() {
         let target = data[i];
         let candidates = &data[i - preamble_len..i];
@@ -20,7 +20,7 @@ fn do_work(data: &[u32], preamble_len: usize) -> Option<u32> {
     None
 }
 
-fn is_sum(target: u32, candidates: &[u32]) -> bool {
+fn is_sum(target: u64, candidates: &[u64]) -> bool {
     for (i, first) in candidates.iter().enumerate() {
         let second_candidates = &candidates[i + 1..];
         for (_, second) in second_candidates.iter().enumerate() {
