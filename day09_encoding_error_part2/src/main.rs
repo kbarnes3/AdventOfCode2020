861e56ae6f4ac2d31cce37aae1572334c605721c
Kevin Barnes <kbarnes3@gmail.com>
2020-12-11 16:34:51 -0800

    Solving Day 09 Part 2

--- day09_encoding_error_part2/src/main.rs
+++ day09_encoding_error_part2/src/main.rs
@@ -10,6 +10,20 @@ fn main() {
 }
 
 fn do_work(data: &[u64], preamble_len: usize) -> Option<u64> {
+    let invalid_number = find_invalid_number(data, preamble_len);
+    if let Some(invalid_number) = invalid_number {
+        let sum_range = find_contiguous_sum(data, invalid_number);
+        if let Some(range) = sum_range {
+
+            let sum = range.iter().min().unwrap() + range.iter().max().unwrap();
+            return Some(sum);
+        }
+    }
+
+    None
+}
+
+fn find_invalid_number(data: &[u64], preamble_len: usize) -> Option<u64> {
     for i in preamble_len..data.len() {
         let target = data[i];
         let candidates = &data[i - preamble_len..i];
@@ -31,4 +45,29 @@ fn is_sum(target: u64, candidates: &[u64]) -> bool {
     }
 
     false
+}
+
+fn find_contiguous_sum(data: &[u64], target: u64) -> Option<&[u64]> {
+    for i in 0..data.len() - 1 {
+        let mut j = i + 1;
+        let mut sum = data[i] + data[j];
+
+        loop {
+            if sum == target {
+                return Some(&data[i..=j]);
+            }
+
+            j += 1;
+            if j >= data.len() {
+                break;
+            }
+
+            sum += data[j];
+            if sum > target {
+                break;
+            }
+        }
+    }
+
+    None
 }
