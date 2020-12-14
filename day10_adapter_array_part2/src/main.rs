c9499c75827605b45a2fefa1ff26aba27df2b47e
Kevin Barnes <kbarnes3@gmail.com>
2020-12-13 20:47:08 -0800

    Solving Day 10 Part 2

--- day10_adapter_array_part2/src/main.rs
+++ day10_adapter_array_part2/src/main.rs
@@ -1,3 +1,4 @@
+use std::vec::Vec;
 #[allow(unused_imports)]
 use day10_adapter_array_common::{SAMPLE_DATA_1, SAMPLE_DATA_2, REAL_DATA};
 
@@ -6,25 +7,40 @@ fn main() {
     println!("{}", result);
 }
 
-fn do_work(data: &[u32]) -> u32 {
+fn do_work(data: &[u32]) -> u64 {
     let mut adapters = data.to_vec();
+    adapters.push(0);
     adapters.sort();
+
     let biggest_adapter = adapters[adapters.len() - 1];
 
-    adapters.insert(0, 0);
-    adapters.insert(adapters.len(), biggest_adapter + 3);
+    adapters.push(biggest_adapter + 3);
 
-    let mut one_differences = 0;
-    let mut three_differences = 0;
+    let mut possible_routes = Vec::with_capacity(adapters.len());
+    possible_routes.push(1);
 
-    for i in 0..adapters.len() - 1 {
-        let difference  = adapters[i + 1] - adapters[i];
-        if difference == 1 {
-            one_differences += 1;
-        } else if difference == 3 {
-            three_differences += 1;
+    for i in 1..adapters.len() {
+        let current_joltage = adapters[i];
+        let mut current_routes = 0;
+        let mut j = i - 1;
+
+        loop {
+            let prev_joltage = adapters[j];
+            if prev_joltage + 3 >= current_joltage {
+                current_routes += possible_routes[j];
+            } else {
+                break;
+            }
+
+            if j > 0 {
+                j -= 1;
+            } else {
+                break;
+            }
         }
+
+        possible_routes.push(current_routes);
     }
 
-    one_differences * three_differences
+    possible_routes[possible_routes.len() - 1]
 }
