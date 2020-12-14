e681d133daa2c544651bac4396f7b587d63e4134
Kevin Barnes <kbarnes3@gmail.com>
2020-12-13 19:09:53 -0800

    Day 10 Part 1 works with sample data

--- day10_adapter_array_part1/src/main.rs
+++ day10_adapter_array_part1/src/main.rs
@@ -1,7 +1,30 @@
 #[allow(unused_imports)]
-use day10_adapter_array_common::{HELLO};
+use day10_adapter_array_common::{SAMPLE_DATA_1, SAMPLE_DATA_2};
 
 fn main() {
-    println!("Hello, world!");
+    let result = do_work(&SAMPLE_DATA_2);
+    println!("{}", result);
 }
 
+fn do_work(data: &[u32]) -> u32 {
+    let mut adapters = data.to_vec();
+    adapters.sort();
+    let biggest_adapter = adapters[adapters.len() - 1];
+
+    adapters.insert(0, 0);
+    adapters.insert(adapters.len(), biggest_adapter + 3);
+
+    let mut one_differences = 0;
+    let mut three_differences = 0;
+
+    for i in 0..adapters.len() - 1 {
+        let difference  = adapters[i + 1] - adapters[i];
+        if difference == 1 {
+            one_differences += 1;
+        } else if difference == 3 {
+            three_differences += 1;
+        }
+    }
+
+    one_differences * three_differences
+}
