1b640203f2989c8f81137afcd1b4a42ae368d185
Kevin Barnes <kbarnes3@gmail.com>
2020-12-19 16:51:01 -0800

    Day 12 Part 1 sample data works

--- day12_rain_risk_common/src/lib.rs
+++ day12_rain_risk_common/src/lib.rs
@@ -1,1 +1,21 @@
-pub const HELLO: bool = true;
+pub enum Instruction {
+    North(i32),
+    South(i32),
+    East(i32),
+    West(i32),
+    Left(i32),
+    Right(i32),
+    Forward(i32)
+}
+
+use Instruction::*;
+
+// Substitute with:
+// '<,'>s/^/    / | '<,'>s/$/),/ | '<,'>s/N/North(/e | '<,'>s/S/South(/e | '<,'>s/E/East(/e | '<,'>s/W/West(/e | '<,'>s/L/Left(/e | '<,'>s/R/Right(/e | '<,'>s/F/Forward(/e
+pub const SAMPLE_DATA: [Instruction; 5] = [
+    Forward(10),
+    North(3),
+    Forward(7),
+    Right(90),
+    Forward(11),
+];
