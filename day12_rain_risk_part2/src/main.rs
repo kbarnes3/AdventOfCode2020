25eb1285ba601ad97a0d770719d8bbcc46a0369d
Kevin Barnes <kbarnes3@gmail.com>
2020-12-19 16:53:14 -0800

    Starting Day 12 Part 2

---
+++ day12_rain_risk_part2/src/main.rs
@@ -0,0 +1,105 @@
+#[allow(unused_imports)]
+use day12_rain_risk_common::{Instruction, SAMPLE_DATA, REAL_DATA};
+use boat::Boat;
+
+fn main() {
+    let result = do_work(&REAL_DATA);
+    println!("{}", result);
+}
+
+fn do_work(data: &[Instruction]) -> i32 {
+    let mut boat = Boat::new();
+
+    for instruction in data.iter() {
+        boat.process_instruction(instruction);
+    }
+    
+    let coordinates = boat.get_coordinates();
+    coordinates.0.abs() + coordinates.1.abs()
+}
+
+mod boat {
+    use day12_rain_risk_common::Instruction;
+
+    #[derive(Copy, Clone)]
+    enum Bearing {
+        North,
+        South,
+        East,
+        West
+    }
+
+    #[derive(Copy, Clone)]
+    enum TurnDirection {
+        Left,
+        Right
+    }
+
+    pub struct Boat {
+        bearing: Bearing,
+        x_pos: i32,
+        y_pos: i32
+    }
+
+    impl Boat {
+        pub fn new() -> Boat {
+            Boat {
+                bearing: Bearing::East,
+                x_pos: 0,
+                y_pos: 0
+            }
+        }
+
+        pub fn process_instruction(&mut self, instruction: &Instruction) {
+            match instruction {
+                Instruction::North(distance) => self.move_direction(Bearing::North, *distance),
+                Instruction::South(distance) => self.move_direction(Bearing::South, *distance),
+                Instruction::East(distance) => self.move_direction(Bearing::East, *distance),
+                Instruction::West(distance) => self.move_direction(Bearing::West, *distance),
+                Instruction::Left(degrees) => self.turn_ship(TurnDirection::Left, *degrees),
+                Instruction::Right(degrees) => self.turn_ship(TurnDirection::Right, *degrees),
+                Instruction::Forward(distance) => self.move_forward(*distance),
+            }
+        }
+
+        pub fn get_coordinates(&self) -> (i32, i32) {
+            (self.x_pos, self.y_pos)
+        }
+
+        fn move_direction(&mut self, direction: Bearing, distance: i32) {
+            match direction {
+                Bearing::North => self.y_pos -= distance,
+                Bearing::South => self.y_pos += distance,
+                Bearing::East => self.x_pos -= distance,
+                Bearing::West => self.x_pos += distance
+            }
+        }
+
+        fn move_forward(&mut self, distance: i32) {
+            self.move_direction(self.bearing, distance);
+        }
+
+        fn turn_ship(&mut self, direction: TurnDirection, degrees: i32) {
+            let mut degrees = degrees;
+
+            while degrees != 0 {
+                if degrees < 0 {
+                    panic!("Non-90 degree turn!");
+                }
+
+                match (direction, self.bearing) {
+                    (TurnDirection::Left, Bearing::North) => self.bearing = Bearing::West,
+                    (TurnDirection::Left, Bearing::East) => self.bearing = Bearing::North,
+                    (TurnDirection::Left, Bearing::South) => self.bearing = Bearing::East,
+                    (TurnDirection::Left, Bearing::West) => self.bearing = Bearing::South,
+                    (TurnDirection::Right, Bearing::North) => self.bearing = Bearing::East,
+                    (TurnDirection::Right, Bearing::East) => self.bearing = Bearing::South,
+                    (TurnDirection::Right, Bearing::South) => self.bearing = Bearing::West,
+                    (TurnDirection::Right, Bearing::West) => self.bearing = Bearing::North,
+                }
+
+                degrees -= 90;
+            }
+        }
+    }
+}
