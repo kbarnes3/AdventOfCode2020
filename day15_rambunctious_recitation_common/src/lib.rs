096b4895747c1990331122cc2c9bbbe434697f94
Kevin Barnes <kbarnes3@gmail.com>
2020-12-23 18:51:57 -0800

    Day 15 Part 1 works with sample data

--- day15_rambunctious_recitation_common/src/lib.rs
+++ day15_rambunctious_recitation_common/src/lib.rs
@@ -1,1 +1,2 @@
-pub const HELLO: bool = true;
+pub const ITERATIONS: usize = 2020;
+pub const SAMPLE_DATA: [usize; 3] = [0,3,6 ];
--- day15_rambunctious_recitation_part1/src/main.rs
+++ day15_rambunctious_recitation_part1/src/main.rs
@@ -1,6 +1,32 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
-use day15_rambunctious_recitation_common::{HELLO};
+use day15_rambunctious_recitation_common::{ITERATIONS, SAMPLE_DATA};
 
 fn main() {
-    println!("Hello, world!");
+    let result = do_work(&SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work(starting_data: &[usize]) -> usize {
+    let mut spoken_numbers = HashMap::new();
+    let mut last_spoken = 0;
+
+    for round in 1..=starting_data.len() {
+        last_spoken = starting_data[round - 1];
+        spoken_numbers.insert(last_spoken, round);
+    }
+
+    for round in starting_data.len()+1..=ITERATIONS {
+        let next_spoken;
+        if spoken_numbers.contains_key(&last_spoken) {
+            let last_spoken_in = spoken_numbers[&last_spoken];
+            next_spoken = round - last_spoken_in - 1;
+        } else {
+            next_spoken = 0;
+        }
+        spoken_numbers.insert(last_spoken, round - 1);
+        last_spoken = next_spoken;
+    }
+
+    last_spoken
 }
