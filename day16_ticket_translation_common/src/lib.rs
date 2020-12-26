808f91955ec4a47a6824ff8c607a0a6a068a3afc
Kevin Barnes <kbarnes3@gmail.com>
2020-12-25 23:14:03 -0800

    Day 16 Part 1 sample data works

--- day16_ticket_translation_common/src/lib.rs
+++ day16_ticket_translation_common/src/lib.rs
@@ -1,1 +1,39 @@
-pub const HELLO: bool = true;
+pub struct ConsecutiveRange {
+    pub min: u64,
+    pub max: u64
+}
+
+pub struct Rule {
+    pub name: &'static str,
+    pub ranges: &'static [ConsecutiveRange],
+}
+
+pub struct Ticket {
+    pub fields: &'static [u64],
+}
+
+pub struct Notes {
+    pub rules: &'static [Rule],
+    pub my_ticket: Ticket,
+    pub nearby_tickets: &'static [Ticket],
+}
+
+// Substitute with:
+// '<,'>s/\(\w\+\): \(\d\+\)-\(\d\+\) or \(\d\+\)-\(\d\+\)/        Rule { name: "\1", ranges: &[ ConsecutiveRange { min: \2, max: \3 }, ConsecutiveRange { min: \4, max: \5 } ] },
+// '<,'>s/\(.*\)/        Ticket { fields: &[ \1 ] },
+pub const SAMPLE_DATA: Notes = Notes {
+    rules: &[
+        Rule { name: "class", ranges: &[ ConsecutiveRange { min: 1, max: 3 }, ConsecutiveRange { min: 5, max: 7 } ] },
+        Rule { name: "row", ranges: &[ ConsecutiveRange { min: 6, max: 11 }, ConsecutiveRange { min: 33, max: 44 } ] },
+        Rule { name: "seat", ranges: &[ ConsecutiveRange { min: 13, max: 40 }, ConsecutiveRange { min: 45, max: 50 } ] },
+    ],
+
+    my_ticket: Ticket { fields: &[ 7,1,14 ] },
+
+    nearby_tickets: &[
+        Ticket { fields: &[ 7,3,47 ] },
+        Ticket { fields: &[ 40,4,50 ] },
+        Ticket { fields: &[ 55,2,20 ] },
+        Ticket { fields: &[ 38,6,12 ] },
+    ],
+};
--- day16_ticket_translation_part1/src/main.rs
+++ day16_ticket_translation_part1/src/main.rs
@@ -1,6 +1,41 @@
 #[allow(unused_imports)]
-use day16_ticket_translation_common::{HELLO};
+use day16_ticket_translation_common::{Rule, Ticket, Notes, SAMPLE_DATA};
 
 fn main() {
-    println!("Hello, world!");
+    let result = do_work(&SAMPLE_DATA);
+    println!("{}", result);
+}
+
+fn do_work(data: &Notes) -> u64 {
+    let mut error_rate = 0;
+
+    for ticket in data.nearby_tickets {
+        error_rate += get_invalid_fields(&data.rules, ticket);
+    }
+
+    error_rate
+}
+
+fn get_invalid_fields(rules: &[Rule], ticket: &Ticket) -> u64 {
+    let mut invalid_fields = 0;
+
+    for field in ticket.fields {
+        if !is_field_valid(rules, *field) {
+            invalid_fields += *field;
+        }
+    }
+
+    invalid_fields
+}
+
+fn is_field_valid(rules: &[Rule], field: u64) -> bool {
+    for rule in rules {
+        for range in rule.ranges {
+            if (field >= range.min) && (field <= range.max) {
+                return true;
+            }
+        }
+    }
+
+    false
 }
