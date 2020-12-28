f4784836be2e9425b4814a78540e59369d3cdf9a
Kevin Barnes <kbarnes3@gmail.com>
2020-12-28 12:14:08 -0800

    Starting Day 16 Part 2

---
+++ day16_ticket_translation_part2/src/main.rs
@@ -0,0 +1,41 @@
+#[allow(unused_imports)]
+use day16_ticket_translation_common::{Rule, Ticket, Notes, SAMPLE_DATA, REAL_DATA};
+
+fn main() {
+    let result = do_work(&REAL_DATA);
+    println!("{}", result);
+}
+
+fn do_work(data: &Notes) -> u64 {
+    let mut error_rate = 0;
+
+    for ticket in data.nearby_tickets {
+        error_rate += get_invalid_fields(&data.rules, ticket);
+    }
+
+    error_rate
+}
+
+fn get_invalid_fields(rules: &[Rule], ticket: &Ticket) -> u64 {
+    let mut invalid_fields = 0;
+
+    for field in ticket.fields {
+        if !is_field_valid(rules, *field) {
+            invalid_fields += *field;
+        }
+    }
+
+    invalid_fields
+}
+
+fn is_field_valid(rules: &[Rule], field: u64) -> bool {
+    for rule in rules {
+        for range in rule.ranges {
+            if (field >= range.min) && (field <= range.max) {
+                return true;
+            }
+        }
+    }
+
+    false
+}
