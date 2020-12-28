bcb0eb9015d1006ea1379fd0b133de22ee612df6
Kevin Barnes <kbarnes3@gmail.com>
2020-12-28 15:21:03 -0800

    Day 17 Part 1 sample data passes

--- day17_conway_cubes_common/src/lib.rs
+++ day17_conway_cubes_common/src/lib.rs
@@ -1,1 +1,17 @@
-pub const HELLO: bool = true;
+#[derive(Copy, Clone)]
+pub enum Cube {
+    Active,
+    Inactive
+}
+
+use Cube::*;
+
+pub const CYCLES: usize = 6;
+
+// Substitute with:
+// '<,'>s/^/    [ / | '<,'>s/$/],/ | '<,'>s/\./Inactive, /g | '<,'>s/#/Active, /g
+pub const SAMPLE_DATA: [[Cube; 3]; 3] = [
+    [ Inactive, Active, Inactive, ],
+    [ Inactive, Inactive, Active, ],
+    [ Active, Active, Active, ],
+];
